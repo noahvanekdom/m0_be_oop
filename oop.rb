59415a03883a1926621be35cab9b51bd085499ac
Noah van Ekdom <noahvanekdom@gmail.com>
2022-06-10 12:04:57 -0600

    Add first attempt at classes

--- oop.rb
+++ oop.rb
@@ -3,13 +3,41 @@
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
 
-
-
+# class Unicorn
+#   def initialize(name)
+#     @name = name
+#     @color = "silver"
+#   end
+#
+#   def say(words)
+#     p "*~* #{words} *~*"
+#   end
+# end
+#
+#
+# Unicorn1 = Unicorn.new("Jimothy")
+#
+# Unicorn1.say("Hello")
 #  Write a class called Vampire
 #  it should have a dynamic name attribute
 #  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
+# class Vampire
+#   def initialize(name, pet = "bat")
+#     @name = name
+#     @pet = pet
+#     @thirsty = true
+#   end
+#   def drink
+#     @thirsty = false
+#   end
+# end
+#
+# Vampire1 = Vampire.new("Tom", "Iguana")
+# p Vampire1
+# Vampire1.drink
+# p Vampire1
 
 
 #  Write a Dragon class
@@ -18,7 +46,36 @@
 #  it should have a dynamic color attribute (string)
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
-
+# class Dragon
+#   def initialize(name, rider, color, is_hungry = true, hunger = 0)
+#     @name = name
+#     @color = color
+#     @rider = rider
+#     @is_hungry = is_hungry
+#     @hunger = hunger
+#   end
+#   def eat
+#     if @hunger == 3
+#       @is_hungry = false
+#       @hunger = 0
+#     else
+#       @hunger = @hunger + 1
+#     end
+#   end
+# end
+#
+# Dragon1 = Dragon.new("Hank","Frank","Red")
+# p Dragon1
+#
+# Dragon1.eat
+# Dragon1.eat
+#
+# p Dragon1
+#
+# Dragon1.eat
+# Dragon1.eat
+#
+# p Dragon1
 
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
@@ -28,3 +85,28 @@
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+class Hobbit
+  def initialize(name, disposition, age = 0)
+    @name = name
+    @disposition = disposition
+    @age = age
+    @is_adult = false
+    @is_old = false
+    @has_ring = false
+    if @age >= 33 && @age < 100
+      @is_adult = true
+    elsif @age > 100
+      @is_old = true
+    else
+    end
+    if @name.include?("Frodo")
+      @has_ring = true
+    end
+  end
+  def celebrate_birthday
+    @age = @age + 1
+  end
+end
+
+Hobbit1 = Hobbit.new("Frodo", "sunny", 44)
+p Hobbit1
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,13 @@
 # Self Evaluation
 
-- This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
-- How is your workflow going, in terms of taking breaks, staying focused, etc.?
-- What do you need to do to be ready for intense technical lessons and challenges like this, for 6 hours/day next week? What support do you need?
+- This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?  
+
+**Felt really good. I definitely did not remember close to everything I needed to, which forced me to find some really helpful resources. I got stuck a few times, took a break, came back and each time was able to make progress. Synthesizing all of the small things we've learned into something that has clear utility is a hugely motivating feeling.**  
+
+- How is your workflow going, in terms of taking breaks, staying focused, etc.?  
+
+**My workflow feels good, I'm noticing that I'm avoiding a lot of the issues I've had in my past job managing campaigns, where it feels impossible to get back into the right mindset once you've left it because of how many spinning plates there are. Here I can just put everything down and let my subconscious break down the problem for a bit.**  
+
+- What do you need to do to be ready for intense technical lessons and challenges like this, for 6 hours/day next week? What support do you need?  
+
+**NEXT WEEK? Just kidding. I feel pretty ready, I need to figure out a way to take less time cooking during the day, or a way to make that time more productive. Sticking to my early rising time is important, which means setting a hard and fast bedtime and scheduling to make sure I can meet it.**
